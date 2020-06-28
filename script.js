2f24bcb7d7de954840a14a57733f96ff93834998
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-28 03:17:29 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
-const modalInfo = document.querySelector(".modalInfo");
+
 
 for (let card of cards) {
   const imageId = card.getAttribute("id");
-  card.addEventListener("click", () => {
+    card.addEventListener("click", () => {
     modalOverlay.classList.add("active");
     modalOverlay.querySelector("img").src = `./assets/${imageId}.png`;
+    modalOverlay.querySelector('h1').textContent = document.querySelector('h1').innerHTML
+    modalOverlay.querySelector('p').textContent = document.querySelector('h1').innerHTML
   });
 }
-
-
 
 document.querySelector(".modal-close").addEventListener("click", () => {
   modalOverlay.classList.remove("active");
