4b591949578f7fcbee3c789c425c644d23ad9763
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-28 14:13:36 -0300

    Update
    manupulação da DOM com Javascrip para o texto dos cards ficarem dinamicos

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ for (let card of cards) {
     card.addEventListener("click", () => {
     modalOverlay.classList.add("active");
     modalOverlay.querySelector("img").src = `./assets/${imageId}.png`;
-    modalOverlay.querySelector('h1').textContent = document.querySelector('h1').innerHTML
-    modalOverlay.querySelector('p').textContent = document.querySelector('h1').innerHTML
+    const modalTitle = card.children[1].textContent
+    modalOverlay.querySelector('h1').innerHTML = modalTitle
+    const modalAuthor = card.children[2].textContent
+    modalOverlay.querySelector('p').innerHTML = modalAuthor
   });
 }
 
