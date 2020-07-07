5a1f8fdbe9f03038fdcbf329f9afa4fd0d427eb7
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-07 19:46:33 -0300

    Ajustes pastas e rotas

--- routes.js
+++ routes.js
@@ -1,10 +1,39 @@
-const routes = require("./routes");
+const express = require("express");
+const routes = express.Router();
+const recipes = require("./data");
 
-routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
-routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
-routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+// routes.get("/admin/recipes", recipes.index);
+// routes.get("/admin/recipes/create", recipes.create);
+// routes.get("/admin/recipes/:id", recipes.show);
+// routes.get("/admin/recipes/:id/edit", recipes.edit);
 
-routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-routes.put("/admin/recipes", recipes.put); // Editar uma receita
-routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+// routes.post("/admin/recipes", recipes.post);
+// routes.put("/admin/recipes", recipes.put);
+// routes.delete("/admin/recipes", recipes.delete);
+
+routes.get("/", (req, res) => {
+  return res.render("index", { items: recipes });
+});
+
+routes.get("/about", (req, res) => {
+  return res.render("about");
+});
+
+routes.get("/recipes", (req, res) => {
+  return res.render("recipes", { items: recipes });
+});
+
+routes.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+
+  const recipe = recipes.find((recipe) => {
+    return recipe.id == id;
+  });
+  if (!recipe) {
+    return res.status(404).render("notfound");
+  }
+
+  return res.render("recipe", { item: recipe });
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-const recipes = require("./data");
-const routes = express.Router();
+const routes = require("./routes");
 
 const server = express();
 
-server.set("view engine", "njk");
-server.use(routes);
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
+server.use(routes);
+
+server.set("view engine", "njk");
 
 nunjucks.configure("views", {
   express: server,
@@ -15,33 +16,6 @@ nunjucks.configure("views", {
   noCache: true,
 });
 
-server.get("/", (req, res) => {
-  return res.render("index", { items: recipes });
-});
-
-server.get("/about", (req, res) => {
-  return res.render("about");
-});
-
-server.get("/recipes", (req, res) => {
-  return res.render("recipes", { items: recipes });
-});
-
-server.get("/recipe/:id", (req, res) => {
-  const id = req.params.id;
-
-  const recipe = recipes.find((recipe) => {
-    return recipe.id == id;
-  });
-  if (!recipe) {
-    return res.status(404).render("notfound");
-  }
-
-  return res.render("recipe", { item: recipe });
-});
-
 server.listen(5000, () => {
   console.log("server is running");
 });
-
-module.exports = routes;
