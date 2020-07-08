f29df1c640ff760978b6c9aab7e3b8e2e33b4972
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-08 20:51:25 -0300

    adjustes of fields in form

---
+++ public/admin.js
@@ -0,0 +1,32 @@
+function addIngredient() {
+  const ingredients = document.querySelector("#ingredients");
+  const fieldContainer = document.querySelectorAll(".ingredient");
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+document
+  .querySelector(".add-ingredient")
+  .addEventListener("click", addIngredient);
+
+function addPrepare() {
+  const prepares = document.querySelector("#prepares");
+  const fieldContainer = document.querySelectorAll(".prepare");
+
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  if (newField.children[0].value == "") return false;
+
+  newField.children[0].value = "";
+  prepares.appendChild(newField);
+}
+
+document.querySelector(".add-prepare").addEventListener("click", addPrepare);
