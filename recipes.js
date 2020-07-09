e916e7703fd91cef5346eccb626af1b6ff975d5d
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-09 19:01:57 -0300

    create delete method and adjustes buttons

--- recipes.js
+++ recipes.js
@@ -61,3 +61,40 @@ exports.edit = function (req, res) {
   };
   return res.render("./admin/edit", { recipe });
 };
+
+exports.put = function (req, res) {
+  const { id } = req.body;
+  let index = 0;
+  const foundRecipe = data.recipes.find(function (recipe, foundRecipe) {
+    if (id == recipe.id) {
+      index = foundRecipe;
+      return true;
+    }
+  });
+  if (!foundRecipe) return res.send("Receita não encontrada");
+  const recipe = {
+    ...foundRecipe,
+    ...req.body,
+  };
+  data.recipes[index] = recipe;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("Erro no arquivo");
+
+    return res.redirect(`/admin/recipes/${id}`);
+  });
+};
+
+exports.delete = function (req, res) {
+  const { id } = req.body;
+  const filterRecipe = data.recipes.filter(function (recipe) {
+    return recipe.id != id;
+  });
+  data.recipes = filterRecipe;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("Falha no arquivo");
+
+    return res.redirect("/admin/recipes");
+  });
+};
--- routes.js
+++ routes.js
@@ -10,8 +10,8 @@ routes.get("/admin/recipes/:id", recipes.show);
 routes.get("/admin/recipes/:id/edit", recipes.edit);
 
 routes.post("/admin/recipes", recipes.post);
-// routes.put("/admin/recipes", recipes.put);
-// routes.delete("/admin/recipes", recipes.delete);
+routes.put("/admin/recipes", recipes.put);
+routes.delete("/admin/recipes", recipes.delete);
 
 routes.get("/", (req, res) => {
   return res.render("index", { recipes: data.recipes });
