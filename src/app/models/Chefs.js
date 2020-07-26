cbd762386a4b8dc3b51cf2c790225a0349c59ca8
lfeliperibeiro <piporibeiro@gmail.com>
2020-07-25 21:15:13 -0300

    create count logic

--- src/app/models/Chefs.js
+++ src/app/models/Chefs.js
@@ -34,13 +34,16 @@ module.exports = {
   },
   find(id, callback) {
     db.query(
-      `
-    SELECT * FROM chefs WHERE id= $1`,
+      `SELECT chefs.*, recipes.id AS total_recipes
+      FROM chefs
+      LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
+      WHERE chefs.id= $1
+    `,
       [id],
       (err, results) => {
         if (err) throw `Database error ${err}`;
 
-        callback(results.rows[0]);
+        callback(results.rows[0], results.rowCount);
       }
     );
   },
