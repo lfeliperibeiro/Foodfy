ce8322b1edf40d94d52f5c7a348fc1686ed5e66c
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-17 20:47:49 -0300

    update chefs

--- src/app/controllers/chefs.js
+++ src/app/controllers/chefs.js
@@ -13,14 +13,14 @@ module.exports = {
     return response.render("admin/chefs/create");
   },
   post(request, response) {
-    const keys = Object.keys(req.body);
+    const keys = Object.keys(request.body);
     for (key of keys) {
-      if (req.body[key] == "") {
-        return res.send("Por favor preencha todos os campos");
+      if (request.body[key] == "") {
+        return response.send("Por favor preencha todos os campos");
       }
     }
     Chefs.create(request.body, (chefs) => {
-      return response.redirect('admin/chefs');
+      return response.redirect('/admin/chefs');
     });
   },
   show(request, response) {
@@ -35,24 +35,24 @@ module.exports = {
   edit(request, response) {
     const {id} = request.params
     Chefs.find(id, (chef) => {
-     return res.render("admin/chefs/edit", { chef });
+     return response.render("admin/chefs/edit", { chef });
     });
   },
   put(request, response) {
-    const keys = Object.keys(req.body);
+    const keys = Object.keys(request.body);
     for (key of keys) {
-      if (req.body[key] == "") {
-        return res.send("Por favor preencha todos os campos");
+      if (request.body[key] == "") {
+        return response.send("Por favor preencha todos os campos");
       }
     }
     Chefs.update(request.body, (chef) => {
-      return res.redirect('/admin/chefs');
+      return response.redirect('/admin/chefs');
     });
   },
   delete(request, response) {
     const {id} = request.body
     Chefs.delete(id, (chefs) => {
-      return res.redirect("/admin/chefs");
+      return response.redirect("/admin/chefs");
     });
   },
 };
